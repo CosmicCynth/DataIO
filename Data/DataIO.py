eb55143885dfd3afcf9f9809c42b18706d83398f
CosmicCynth <theshinyseal@gmail.com>
2024-11-12 09:05:32 +0100

    DOne

--- Data/DataIO.py
+++ Data/DataIO.py
@@ -1,4 +1,10 @@
+namesBelow5 = []
+
 with open('lastnames.txt','r') as fil:
     out = fil.read().split('\n')
-print(out)
 
+for i in out:
+    if len(i) > 5: # You change as you please
+        namesBelow5.append(i)
+
+print(namesBelow5)
