3d2c0a495d3453fff70f1a1fd3484dbc78be5cdb
CosmicCynth <theshinyseal@gmail.com>
2024-11-12 09:46:18 +0100

    :)

--- Data/Write.py
+++ Data/Write.py
@@ -1,7 +1,24 @@
-#with open('beerbottlesong.txt','w') as fil:
+
+
 BeerBottles = 99
+Song = ""
 
 while BeerBottles > 0:
-    print(str(BeerBottles) + " Bottles of beer on the wall " + str(BeerBottles) + " Bottles of beer. Take one down and pass it around, ")
+
+    if BeerBottles == 1:
+        bottle_word = "Bottle"
+    else:
+        bottle_word = "Bottles"
+
+    Song+=(str(BeerBottles)+ " " + bottle_word + " of beer on the wall " + str(BeerBottles)+" " + bottle_word + " of beer. Take one down and pass it around, ")
     BeerBottles = BeerBottles - 1
-    print(str(BeerBottles) + " Bottles of beer on the wall.")
+
+    if BeerBottles == 1:
+        bottle_word = "Bottle"
+    else:
+        bottle_word = "Bottles"
+
+    Song+=(str(BeerBottles) + " " + bottle_word + " of beer on the wall.\n")
+
+with open('beerbottlesong.txt','w') as fil:
+    fil.write(Song)
