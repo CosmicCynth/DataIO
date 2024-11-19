5ca0179d40d8e3a4f5b4c3b9828a04d4a622f890
CosmicCynth <theshinyseal@gmail.com>
2024-11-19 10:14:49 +0100

    Update MiniProject.py

--- Data/MiniProject.py
+++ Data/MiniProject.py
@@ -6,21 +6,36 @@ print(loripsum.text)
 
 
 AP = loripsum.text.count(".")
+AP = AP+loripsum.text.count("?")
+AP = AP+loripsum.text.count("!")
 WordList = loripsum.text.split()
-Words = len(WordList)
 Longwords = 0
-
+Words = len(WordList)
 
 for i in WordList:
     if len(i) > 6:
         Longwords += 1
 
-print("Punktumer: " +str(AP), "Antal ord: " + str(Words), "Lange ord: " + str(Longwords))
+print("Sætninger: " +str(AP), "Antal ord: " + str(Words), "Lange ord: " + str(Longwords))
 
 
-LixTal = Words/AP+Longwords*100/Words
+LixTal = (Words/AP)+(Longwords*100/Words)
 Lixtal = round(LixTal)
 
 print("Lix tal er " + str(Lixtal))
 
+def difficulty():
+    Difficulty = ""
+    if Lixtal <= 24:
+        Difficulty = "Let"
+    if Lixtal > 24 < 35:
+        Difficulty = "Let for øvede læsere"
+    if Lixtal > 34 < 45:
+        Difficulty = "Middel"
+    if Lixtal > 44 < 55:
+        Difficulty = "Svær"
+    if Lixtal > 55:
+        Difficulty = "Meget svær"
+    return(Difficulty)
 
+print("Sværhedsgrad: " + str(difficulty()))
