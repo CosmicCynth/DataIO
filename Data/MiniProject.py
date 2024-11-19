9ffabe42b8adc9d6ef8c56e3ceeb9f3c409debc0
CosmicCynth <theshinyseal@gmail.com>
2024-11-19 09:20:27 +0100

    Create MiniProject.py
    Heheha

---
+++ Data/MiniProject.py
@@ -0,0 +1,17 @@
+AP = loripsum.text.count(".")
+WordList = loripsum.text.split()
+Words = len(WordList)
+Longwords = 0
+
+
+for i in WordList:
+    if len(i) > 6:
+        Longwords += 1
+
+print("Punktumer: " +str(AP), "Antal ord: " + str(Words), "Lange ord: " + str(Longwords))
+
+
+LixTal = Words/AP+Longwords*100/Words
+Lixtal = round(LixTal)
+
+print("Lix tal er " + str(Lixtal))
